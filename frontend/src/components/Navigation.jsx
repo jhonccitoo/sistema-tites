1c07c61001f68a23e02671406e0c97288f56b97c
jhonccitoo <129339472+jhonccitoo@users.noreply.github.com>
2025-04-28 15:41:15 -0500

    mern9

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 export default class Navigation extends Component {
   render() {
-    return <div>Navigation</div>;
+    return ( <>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="/">NotesApp</Navbar.Brand>
+          <Nav className="me-right">
+            <Nav.Link href="/">Notes</Nav.Link>
+            <Nav.Link href="/create">Create Note</Nav.Link>
+            <Nav.Link href="/user">Create User</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>);
   }
 }
