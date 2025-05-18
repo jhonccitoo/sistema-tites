33fa13d1f2ba3ac809b576d5fe8fe81eee3532fc
jhonccitoo <129339472+jhonccitoo@users.noreply.github.com>
2025-05-18 12:08:37 -0500

    eliminando modulos

--- backend/src/database.js
+++ backend/src/database.js
@@ -4,8 +4,8 @@ require('dotenv').config(); // Carga .env si es necesario aquí
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, )
-  .then(() => console.log('a'))
+mongoose.connect(uri)
+  .then(() => console.log('Database connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // No necesitas exportar nada si solo quieres ejecutar la conexión.
